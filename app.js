d6a68f3ce0fbdfc249c30f67709fcbb0c53e00e0
infocyenosure <145680083+infocyenosure@users.noreply.github.com>
2023-09-24 16:14:49 +0530

    Update app.js
    another route

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send("hello")
 })
+app.get('/home',(req,res)=>{
+    res.send("home")
+})
 
 app.listen(port,()=>{
     console.log(`server running ${port}`);
